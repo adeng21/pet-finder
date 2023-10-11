3fa714d132dcef8b5a3d2387e2473f62134015f5
Albert Deng <adeng21@gmail.com>
2023-10-10 22:47:56 -0700

    try

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home({dogsData}) {
     return (
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 class="text-2xl font-bold tracking-tight text-gray-900">Katie's Pet Finder</h2>
+        <h2 class="text-2xl font-bold tracking-tight text-gray-900">Katies Pet Finder</h2>
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {dogsData.map((dog)=> (
